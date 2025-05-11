fd9788a453c5103db5e8e5e81a9ca6c56d11425c
lzt <1162300237@qq.com>
2025-05-11 23:12:04 +0800

    feat: update shared

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -6,4 +6,14 @@ export function isFunction(val: any): val is Function {
   return typeof val === 'function'
 }
 
+export function isString(val: any): val is string {
+  return typeof val === 'string'
+}
+
+export function isArray(val: any): val is any[] {
+  return Array.isArray(val)
+}
+
 export const extend = Object.assign
+
+export * from './shapeFlags'
